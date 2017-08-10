29692d8cb8b493672562b2ecf82b9228f88919d7
David Bartholomew <dbartholomew13@apu.edu>
2017-08-10 09:49:32 -0700

    Adds simple database stuff

--- sidekick/homebase/views.py
+++ sidekick/homebase/views.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 
+from .models import Employee
 
 def index(request):
-    return render(request, 'homebase/index.html')
+    employee_list = Employee.objects.all()
+    context = {'employee_list':employee_list}
+    return render(request, 'homebase/index.html', context)
