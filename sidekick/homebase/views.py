5e4b8ff6953bc9dfc74d2c1e4d513c0024299454
David Bartholomew <dbartholomew13@apu.edu>
2017-08-31 13:19:13 -0700

    Adds some css and fixes

--- sidekick/homebase/views.py
+++ sidekick/homebase/views.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from sidekick import views
-
+from homebase.models import Announcements
 
 def index(request):
-    context = {}
-    return views.load_page(request, 'homebase/index.html', context)
+    return views.load_page(request, 'homebase/index.html', prep_context())
+
+def prep_context():
+    announcement_list = Announcements.objects.all()
+    return {
+        'announcement_list': announcement_list
+    }
