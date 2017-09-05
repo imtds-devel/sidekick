905c8293e2d16f91011b65c21b9930de2c484a48
Joshua Wood <jwood@localhost.localdomain>
2017-09-05 14:33:22 -0700

    Better javascript, copy to clipboard works and the added quote items have information passed into them

--- sidekick/static/js/quotes.js
+++ sidekick/static/js/quotes.js
@@ -1,3 +1,4 @@
+var addService;
 $(document).ready(function() {
     // When the "Reset Quotes" button is pressed, 
     $('#reset-button').click(function(){
@@ -6,12 +7,9 @@ $(document).ready(function() {
     })
 
     // When the "Copy Quote to Clipboard" button is pressed 
-    // #TODO - FIX 
     $('#clipboard-button').click(function(){
         // Select the text in the quote        
-        document.querySelector('#text-quote').querySelectorAll('p').select();
-        document.execCommand('copy');
-        
+        document.querySelector('#text-quote').select();        
         // The copy is in a try because some browsers do not support the feature to copy this way
         try {
             document.execCommand('copy');
@@ -19,25 +17,25 @@ $(document).ready(function() {
             alert("Sorry, your browser does not support this feature!")
         }
     });
+        
     // When a service is clicked
-    $('#service').click(function(){
+    addService = function(serviceName, servicePrice){
 
         // Adds the selected quote as a button
-        $('#quote-items').append("<button id='service' type='button' class='btn btn-block service-btn'>"
+        $('#quote-items').append("<button type='button' class='btn btn-block service-btn'>"
         +"<div class ='media'>"
            +"<div class ='media-left'>"
-            +"{{service.service}}"
+            +serviceName
             +"</div>"
-            +"<div class ='media-body'>"
-            +"{{service.description}}"
+            +"<div class ='media-center'>"
+            +"$" + servicePrice
             +"</div>"
             +"<div class ='media-right'>"
-            +"${{service.price}}"
-            +"</div>"
+            +"<button id ='remove-service' type='button' class='btn btn-danger'>X</button></div>"
         +"</div>"
     +"</button>");
         
         // Adds the selected quote as text
-        $('#text-quote').append($("<p></p>").text("Quote Item $100"));
-    });
+        $('#text-quote').append(serviceName);
+    };
 });
