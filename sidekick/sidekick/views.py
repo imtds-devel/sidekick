11a5e03da9f6ae3393c44db6bf6ca2ee816c3ecf
Nico Chera <nchera13@apu.edu>
2017-09-28 14:09:11 -0700

    Authorization test

--- sidekick/sidekick/views.py
+++ sidekick/sidekick/views.py
@@ -4,11 +4,21 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from roster.models import Trophies
+from homebase.models import Employees
 
 
 @login_required
 def load_page(request, template, context):
+
+    # Check to make sure authenticated user is authorized to access the webpage
+    if not authorize(request):
+        return "403 unauthorized user!"
+
     trophy_list = Trophies.objects.all()
     context['trophy_list'] = trophy_list
 
     return render(request, template, context)
+
+
+def authorize(request):
+    return Employees.objects.get(netid=request.user) is not None
