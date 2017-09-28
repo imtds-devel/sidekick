8ce571868948e203c73e1e4ab0e8a6df7993d66c
root <root@localhost.localdomain>
2017-09-28 16:57:34 -0400

    authenticationgit status!

--- sidekick/homebase/views.py
+++ sidekick/homebase/views.py
@@ -2,7 +2,7 @@
 from sidekick import views
 from django.contrib.auth.decorators import login_required
 
-@login_required
+# @login_required
 def index(request):
     context = {}
     return views.load_page(request, 'homebase/index.html', context)
--- sidekick/sidekick/views.py
+++ sidekick/sidekick/views.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from roster.models import Trophies
 
 
-
+@login_required
 def load_page(request, template, context):
     trophy_list = Trophies.objects.all()
     context['trophy_list'] = trophy_list
