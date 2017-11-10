3a9cecb284b624cda93f1d915f2025c1fea98816
Bobo <bduggan14@apu.edu>
2017-11-09 17:57:42 -0800

    fixed up internal meta data, now just need to make one modal

--- sidekick/static/js/roster.js
+++ sidekick/static/js/roster.js
@@ -6,14 +6,17 @@
 $('.btn').click(function showModal(){
 
 unformatted_info = $(this).find("ul")
-formatted_info = $(this).find("il")
+formatted_info = $(this).find("li")
 
 netid_1 = $(this).find(".emp-meta").attr('id')
-fname = $(netid_1).find(il).id(".emp-fname").text()
+fullname = $(this).find(".m-fullname").text()
+codename = $(this).find(".m-code").text()
+position = $(this).find(".m-position").text()
+phone = $(this).find(".m-phone").text()
+birthday = $(this).find(".m-birth").text()
 
 netid_1 = netid_1.slice(2)
-console.log(fname)
-console.log(netid_1)
+console.log(fullname, netid_1, codename, position, phone, birthday)
 
 $('#'+ netid_1).modal('show');
 
