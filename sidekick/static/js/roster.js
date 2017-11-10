6dc97b59263be1753178294b38fd7d1fa3b4f413
Bobo <bduggan14@apu.edu>
2017-11-10 11:31:50 -0800

    new modals

--- sidekick/static/js/roster.js
+++ sidekick/static/js/roster.js
@@ -14,9 +14,37 @@ codename = $(this).find(".m-code").text()
 position = $(this).find(".m-position").text()
 phone = $(this).find(".m-phone").text()
 birthday = $(this).find(".m-birth").text()
+pic = $(this).find(".m-pic").text()
+color = $(this).find(".m-poscol").text()
+dev = $(this).find(".m-dev").text()
 
 netid_1 = netid_1.slice(2)
 console.log(fullname, netid_1, codename, position, phone, birthday)
+
+    $('#drop-down').append("<div id=" + netid_1 + " class='modal fade'>"
+        +"<div class='modal-dialog modal-lg'>"
+        +"<div class='modal-content "+ color +" dev_"+ dev +"'>"
+        +"<div class='modal-header'>"
+        +"<h4 style='text-align: center' class='modal-title'>" + fullname + "</h4></div>"
+        +"<div class='modal-body hero-bio'>"
+        +"<div class='row'>"
+        +"<img src='{% static "+ pic +"|img_exists %}' class='img-circle img-herobio'>"
+        +"<div class='hero-bio'>"
+        +"<p>" + fullname +"</p>"
+        +"<p>" + netid_1 + "</p>"
+        +"<p>" + codename + "</p>"
+        +"<p>" + position + "</p>"
+        +"<p>" + phone + "</p>"
+        +"<p>" + birthday + "</p>"
+        +"</div>"
+        +"</div>"
+        +"</div>"
+        +"<div class='modal-footer'>"
+        +"<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>"
+        +"</div>"
+        +"</div>"
+        +"</div>"
+        +"</div>");
 
 $('#'+ netid_1).modal('show');
 
