3abd8a43d965b6a2c777e69c2f8db2ae7a65d8ff
joshuawoodapu <jwood14@apu.edu>
2017-11-20 11:35:30 -0800

    Modals are now generating for the shifts

--- sidekick/static/js/shifts.js
+++ sidekick/static/js/shifts.js
@@ -7,7 +7,7 @@ weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 
 $(document).ready(function() {
     // On page load we want to load the correct shifts and update the display
-    ajaxWithDate();
+    ajaxWithDate('curr');
 
     function ajaxWithDate(option){
         console.log("Sent")
@@ -61,6 +61,7 @@ $(document).ready(function() {
     });
     function generateShiftPanels(week, shifts){
         $('#your-shifts-panel-group').empty();
+        $('#shift-modals').empty();
         // Loop through the days in the given week
         for (day = 0; day < week.length; day++){
             // Evaluate if we should be displaying this day
@@ -78,9 +79,50 @@ $(document).ready(function() {
                 {
                     // Generate a shift "button" for each shift
                     $('#' + weekDays[day] + '-shifts .panel-body').append(
-                        "<button type = 'button' class = 'btn btn-block shift-btn' data-toggle= 'modal' data-target= '#shift-id' >" + shiftsDay[shift].title + "</button>"                    
+                        "<button type = 'button' class = 'btn btn-block shift-btn' data-toggle= 'modal' data-target= '#" + shiftsDay[shift].id + "' >" + shiftsDay[shift].title + "</button>"                    
                     )
-                    //TODO: Generate modals for those shifts
+                    $('#shift-modals').append(
+                        "<div id = '" + shiftsDay[shift].id + "' class = 'modal fade'>"
+                        + "<div class='modal-dialog modal-lg'>"
+                        +    "<div class='modal-content'>"
+                        +        "<div class='modal-header'>"
+                        +            shiftsDay[shift].title
+                        +            "<button type='button' class='close' data-dismiss='modal'>&times;</button>"
+                        +            "<h4 class='modal-title'></h4>"
+                        +        "</div>"
+                        +        "<div class='modal-body hero-bio'>"
+                        +            "<div class='hero-bio'>"
+                        +                "<button type = 'button' class = 'btn btn-default' data-dismiss = 'modal' data-toggle= 'modal' data-target = '#post-conf-" + String(shiftsDay[shift].id) + "'>Post Cover</button>"
+                        +            "</div>"
+                        +        "</div>"
+                        +        "<div class='modal-footer'>"
+                        +            "<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>"
+                        +        "</div>"
+                        +    "</div>"
+                        +"</div>"
+                    +"</div>"
+                    )
+                    $('#shift-modals').append(
+                        "<div id = 'post-conf-" + String(shiftsDay[shift].id) + "' class = 'modal fade'>"
+                        +"<div class='modal-dialog modal-sm'>"
+                        +   "<div class='modal-content'>"
+                        +       "<div class='modal-header'>"
+                        +       shiftsDay[shift].title
+                        +       "<button type='button' class='close' data-dismiss='modal' data-toggle = 'modal' data-target = '#" + shiftsDay[shift].id + "'>&times;</button>"
+                        +       "<h4 class='modal-title'></h4>"
+                        +   "</div>"
+                        +   "<div class='modal-body'>"
+                        +       "<div id='posting-progress'></div>"
+                        +       "<div id='post-det'>"
+                        +           "<p>Post Cover for " + shiftsDay[shift].owner + " at Marshburn: 10AM - 2PM?</p>"
+                        +              "<button id= 'post-cover-btn' type= 'button' class= 'btn btn-primary'>Post</button>"
+                        +              "<button type='button' class='btn btn-default' data-toggle = 'modal' data-target = '#" + shiftsDay[shift].id + "' data-dismiss='modal'>Cancel</button>"
+                        +          "</div>"
+                        +      "</div>"
+                        +      "</div>"
+                        +   "</div>"
+                        +"</div>"
+                    )                        
                 }
             }
         }
