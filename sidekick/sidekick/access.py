07b552d646c49e390337b83f068a0872da27fcf5
Nico Chera <nchera13@apu.edu>
2017-12-04 22:46:40 -0800

    Defined structure of access rights

--- sidekick/sidekick/access.py
+++ sidekick/sidekick/access.py
@@ -1,53 +1,129 @@
 from homebase.models import Employees
 
-def has_access(netid, access_area):
+
+def has_access(netid, area):
     # we have to do this to get the user object itself since the get call will return a QuerySet
     user = None
     for u in Employees.objects.get(netid=netid):
         user = u
 
-    return Employee_Access.get_access(user.position, access_area, user.developer)
+    pos = user.position
 
-class Employee_Access():
-    def get_access(self, pos, area, is_dev):
-        pos_dict = {
-            "lbt": self.get_lbt_access,
-            "spt": self.get_spt_access,
-            "sst": self.get_sst_access,
-            "llt": self.get_llt_access,
-            "mgr": self.get_mgr_access,
-            "stt": self.get_stt_access,
-            "stm": self.get_stm_access,
-            "dev": self.get_dev_access
-        }
+    pos_fns = {
+        "lbt": get_lbt_access,
+        "spt": get_spt_access,
+        "sst": get_sst_access,
+        "llt": get_llt_access,
+        "mgr": get_mgr_access,
+        "stt": get_stt_access,
+        "stm": get_stm_access,
+        "dev": get_dev_access,
+    }
 
-        return pos_dict[pos](area) or pos_dict["dev"](area) if is_dev else pos_dict[pos](area)
+    return pos_fns[pos](area) if not user.developer else pos_fns[pos](area) or pos_fns["dev"](area)
 
 
-    def get_lbt_access(self, area):
-        return True
+def get_lbt_access(area):
+    access_list = [
+        "homebase_access", "passwords_access",
+        # Passwords page access
+        "passwords_lab",
+        #TODO: Finish filling in
+    ]
+    return area in access_list
 
-    def get_spt_access(self, area):
-        return True
 
-    def get_sst_access(self, area):
-        return True
+def get_spt_access(self, area):
+    access_list = [
+        "homebase_access",
+        #TODO: Finish filling in
+    ]
+    return area in access_list
 
-    def get_llt_access(self, area):
-        return True
 
-    def get_mgr_access(self, area):
-        return True
+def get_sst_access(self, area):
+    access_list = [
+        "homebase_access",
+        # TODO: Finish filling in
+    ]
+    return area in access_list
 
-    def get_stt_access(self, area):
-        return True
 
-    def get_stm_access(self, area):
-        return True
+def get_llt_access(self, area):
+    access_list = [
+        "homebase_access",
+        # TODO: Finish filling in
+    ]
+    return area in access_list
 
-    def get_dev_access(self, area):
-        return True
 
-MASTER_ACCESS_TEMPLATE = {
-    "homebase_"
-}
+def get_mgr_access(self, area):
+    access_list = [
+        "homebase_access",
+        # TODO: Finish filling in
+    ]
+    return area in access_list
+
+
+def get_stt_access(self, area):
+    access_list = [
+        "homebase_access",
+        # TODO: Finish filling in
+    ]
+    return area in access_list
+
+
+def get_stm_access(self, area):
+    access_list = [
+        "homebase_access",
+        # TODO: Finish filling in
+    ]
+    return area in access_list
+
+
+def get_dev_access(self, area):
+    access_list = [
+        "homebase_access",
+        # TODO: Finish filling in
+    ]
+    return area in access_list
+
+MASTER_ACCESS_TEMPLATE = [
+    # Homebase Page
+    "homebase_access",                      # May access homebase page
+
+    #   Homing Beacon
+    "homingbeacon_checkin",                 # Can check employees in
+    "homingbeacon_requestpaper",            # Can request a paper count
+
+    #   Announcements/Events
+    "announcements_canedit",                # Can post, modify, or delete announcements/events
+
+
+    # Passwords Page
+    "passwords_access",                     # Access passwords page
+    "passwords_lab",                        # Access lab passwords
+    "passwords_support",                    # Access support passwords
+    "passwords_manager",                    # Access manager passwords
+    "passwords_dev",                        # Access dev passwords
+    "passwords_all",                        # Access all passwords
+
+
+    # Shift Covers Page
+    "shift_access",                         # Access shift covers page
+    "shift_modpanel",                       # View MOD Panel for shift
+    "shift_postlab",                        # Post any lab tech's covers
+    "shift_postall",                        # Post anyone's covers!
+    "shift_viewlabs",                       # View open lab shifts
+    "shift_viewsupport",                    # View open sd & rc shifts
+    "shift_viewall",                        # View all open shifts
+
+
+    # Roster Page
+    "roster_access",                        # Access roster page
+    "roster_editemp",                       # Modify employee metadata
+    "roster_modemp",                        # Modify comment, discipline, & trophy data
+    #TODO: Finish roster page
+
+    #TODO: Continue defining these for all pages!
+]
