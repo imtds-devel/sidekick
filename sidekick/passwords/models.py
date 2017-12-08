5b9ed5d7f7aafdcf619f721db8ebc5c2a1f14bdb
Nico Chera <nchera13@apu.edu>
2017-12-08 15:43:11 -0800

    Moved passwords model

--- sidekick/passwords/models.py
+++ sidekick/passwords/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Passwords(models.Model):
+    name = models.CharField(max_length=20, default="")
+    passwd = models.TextField(default="")
+    description = models.TextField(default="")
+    permission = models.IntegerField(default=3)
+
+    def __str__(self):
+        return self.name + ": " + self.description
