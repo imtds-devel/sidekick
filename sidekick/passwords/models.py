4edb0db68095a08ba4d58156266d9716a03676a7
Nico Chera <nchera13@apu.edu>
2017-12-08 16:01:29 -0800

    Model updates

--- sidekick/passwords/models.py
+++ sidekick/passwords/models.py
@@ -6,6 +6,7 @@ from django.db import models
 # Create your models here.
 
 class Passwords(models.Model):
+
     name = models.CharField(max_length=20, default="")
     passwd = models.TextField(default="")
     description = models.TextField(default="")
@@ -13,3 +14,21 @@ class Passwords(models.Model):
 
     def __str__(self):
         return self.name + ": " + self.description
+
+
+class PassPermission(models.Model):
+    PASS_PERM_OPTIONS = (
+        ('lab', 'Lab Access'),
+        ('sup', 'Support Access'),
+        ('mgr', 'Manager Access'),
+        ('dev', 'Developer Access'),
+        ('all', 'All access'),
+        ('non', 'No Access (hide)')
+    )
+
+    pass_id = models.ForeignKey(Passwords, on_delete = models.CASCADE)
+    permission = models.CharField(
+        max_length=3,
+        choices=PASS_PERM_OPTIONS,
+        default='non'
+    )
--- sidekick/passwords/views.py
+++ sidekick/passwords/views.py
@@ -2,8 +2,15 @@
 from __future__ import unicode_literals
 
 from sidekick import views
+from sidekick import access
 
 # Create your views here.
 def index(request):
-    context = {}
+    context = prep_context(request)
     return views.load_page(request, 'passwords/index.html', context)
+
+def prep_context(request):
+    context = {}
+    context.lab_access = access.get_access(request.user, "passwords_lab")
+    context.support_access = access.get_access(request.user, "passwords_support")
+    context.mod_access = access.get_access(request.user, "passwords_manager")
