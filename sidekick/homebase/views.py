8f01f83736b340fe981ea5cc18a835f7ebc51828
Nico Chera <nchera13@apu.edu>
2018-03-20 13:38:50 -0700

    Restyled the announcement/event posting modal and started work on the backend (pending migration)

--- sidekick/homebase/views.py
+++ sidekick/homebase/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from sidekick import views
-from django.http import HttpResponseRedirect
-from homebase.models import Announcements, Events, StaffStatus
-from .forms import AnnouncmentForm, EventForm, StatusForm
+from django.http import HttpResponseRedirect, JsonResponse
+from homebase.models import Employees, Announcements, Events, StaffStatus
+from .forms import StatusForm
 from shifts.models import Shifts
 import datetime
 import pytz
@@ -16,21 +16,6 @@ def index(request):
         data = copy.copy(request.POST)
 
         form = None
-        if 'e-form' in request.POST:
-            # In case we're not in production
-            # Remove this line before production!
-            request = views.get_current_user(request)
-            data['announcer'] = str(request.user)
-            form = EventForm(data)
-            print(form.fields)
-
-        if 'a-form' in request.POST:
-            # In case we're not in production
-            # Remove this line before production!
-            request = views.get_current_user(request)
-            data['announcer'] = str(request.user)
-            form = AnnouncmentForm(data)
-            print(form)
 
         if 's-form' in request.POST:
             status = StaffStatus.objects.get(netid=request.POST['netid'])
@@ -44,6 +29,30 @@ def index(request):
             print("Bad form!")
 
     return views.load_page(request, 'homebase/index.html', prep_context())
+
+
+def add_announcement(request):
+    # Reject any non-POST request
+    if request.method != "POST":
+        return JsonResponse({
+            'result': 'failure',
+            'desc': 'Bad request method'
+        }, status=500)
+
+    Announcements(
+        announcer=Employees.objects.get(netid=str(request.user)),
+        subject=request.POST.get('subject', None),
+        body=request.POST.get('body', None)
+    ).save()
+
+    return JsonResponse({
+        'result': 'success',
+        'desc': 'Announcement was added successfully'
+    })
+
+
+def add_event(request):
+    return True
 
 
 def prep_context():
@@ -65,15 +74,11 @@ def prep_context():
     staff_stats = StaffStatus.objects.all().order_by('netid')
 
     ordered_list = order(announcement_list, event_list)
-    a_form = AnnouncmentForm()
-    e_form = EventForm()
     s_form = StatusForm()
     return {
         'lab_shifts': labs,
         'support_shifts': support,
         'ordered_list': ordered_list,
-        'a_form': a_form,
-        'e_form': e_form,
         's_form': s_form,
         'staff_stats': staff_stats,
     }
--- sidekick/static/js/homebase.js
+++ sidekick/static/js/homebase.js
@@ -1,16 +1,47 @@
 
 $(document).ready(function () {
     console.log("Homebase loaded");
-    //TODO: Program the things!
-    $(".e-form").hide();
 
-    $(".create-e").click(function(){
-      $(".a-form").hide();
-      $(".e-form").show();
+
+    //Power the announcement/event menu nav buttons
+    $(".announce-menu").click(function() {
+        var opt = $(this).attr('id');
+        $(".announce-menu").removeClass("active");
+        $(".announce-menu-body").hide(200);
+        $("#"+opt+"-container").show(200);
+
+        $(this).addClass("active");
     });
 
-    $(".create-a").click(function(){
-      $(".e-form").hide();
-      $(".a-form").show();
+    $("#announce-submit").click(function() {
+        //start by verifying submitted data
+        var subj = $("#announce-subject").val();
+        var body = $("#announce-body").val();
+
+        //send data to server
+        $.ajax({
+            method:"POST",
+            dataType:"json",
+            url:"ajax/submitannounce",
+            data: {
+                'subject': subj,
+                'body': body
+            },
+            success: function(data) {
+                if (data.result=="success")
+                    alert("Announcement submitted successfully!");
+                else
+                    alert("Announcement posting failed! Please contact Nico");
+            }
+            error: function(err) {
+                alert("Announcement posting failed! Please contact Nico and give him the error details:\n"+err);
+            }
+        });
+
+        //TODO: Reload page
+    });
+
+    $("#event-submit").click(function() {
+        //start by verifying submitted data
     });
 });
