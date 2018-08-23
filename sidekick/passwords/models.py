4b3cce33a796d5c8b51f90c70c0696e99dab5933
mattaniaha <maytenfsu14@apu.edu>
2018-08-23 12:01:03 -0700

    deleting unused column in passwords

--- sidekick/passwords/models.py
+++ sidekick/passwords/models.py
@@ -8,7 +8,6 @@ class Passwords(models.Model):
     name = models.CharField(max_length=20, default="")
     passwd = models.TextField(default="")
     description = models.TextField(default="")
-    permission = models.IntegerField(default=3)
 
     def __str__(self):
         return self.name + ": " + self.description
