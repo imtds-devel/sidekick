9cd8e4fffc3bfbc4a061513eda8ac651e435e06e
mattaniaha <maytenfsu14@apu.edu>
2018-08-24 08:13:55 -0700

    reverting password migrations

--- sidekick/passwords/models.py
+++ sidekick/passwords/models.py
@@ -9,6 +9,7 @@ class Passwords(models.Model):
     name = models.CharField(max_length=20, default="")
     passwd = models.TextField(default="")
     description = models.TextField(default="")
+    permission = models.IntegerField(default=3)
 
     def __str__(self):
         return self.name + ": " + self.description
